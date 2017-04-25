a8369980d8790c4256cf9ddca06bfc6c44284ad7
gsquibbs <squibbsg@msu.edu>
2017-04-25 16:37:37 -0400

    node server with multiple pages

---
+++ server.js
@@ -0,0 +1,34 @@
+// Require Node's http module and assign it to a variable
+var http = require('http')
+
+/* jokes
+var joke1 = '<h1><p>Knock Knock!</p><p>Whos there?</p><p>Opportunity!</p><p>That is impossible. Opportunity doesn’t come knocking twice!</p>'
+var joke2 = '<h1><p>Knock Knock!</p><p>Whos there?</p><p>beats.</p><p>Beats who?</p><Beats me!</p>'
+var joke3 = '<h1><p>Knock Knock!</p><p>Whos there?</p><p>Spell.</p><p>Spell who?</p><p>W-H-O</p>'
+*/
+var server = http.createServer(function (request, response) {
+  if (request.url === '/') {
+    response.end('<h1>Hello! :)</h1>')
+  } else if (request.url === '/random-joke') {
+    var jokeNumber = Math.random()
+    if (jokeNumber < 0.3) {
+      response.end('<h1><p>Knock Knock!</p><p>Who\'s there?</p><p>Opportunity!</p><p>That is impossible. Opportunity doesn\'t come knocking twice!</p></h1>' + '<h2><a href="http://localhost:' + port + '">Welcome Page</a></h2>')
+    } else if (jokeNumber < 0.7) {
+      response.end('<h1><p>Knock Knock!</p><p>Who\'s there?</p><p>beats.</p><p>Beats who?</p><Beats me!</p></h1>' + '<h2><a href="http://localhost:' + port + '">Welcome Page</a></h2>')
+    } else {
+      response.end('<h1><p>Knock Knock!</p><p>Who\'s there?</p><p>Spell.</p><p>Spell who?</p><p>W-H-O</p></h1>' + '<h2><a href="http://localhost:' + port + '">Welcome Page</a></h2>')
+    }
+  } else if (request.url === '/cuteness') {
+    response.end('<h1>Cutie Kitty!</h1>' + '<img src="http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg" alt="Grey Kitten">' + '<h2><a href="http://localhost:' + port + '">Welcome Page</a></h2>')
+  } else {
+    response.end('<h1>Page Not Found</h1>' + '<h2>The requested URL ' + request.url + ' was not found on this server</h2>')
+  }
+})
+
+// Listen on port 8080, so that we can reach the app at
+// localhost:8080
+var port = process.env.PORT || 8080
+server.listen(port)
+
+// Output a friendly message to the terminal
+console.log('Server running at http://localhost:' + port + '/')
