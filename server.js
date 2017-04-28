1e04c715fd96b9c68500557371f0ab830070f6b1
gsquibbs <squibbsg@msu.edu>
2017-04-28 16:39:36 -0400

    updated homepage links

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 // Require Node's http module and assign it to a variable
 var http = require('http')
 
-/* jokes
-var joke1 = '<h1><p>Knock Knock!</p><p>Whos there?</p><p>Opportunity!</p><p>That is impossible. Opportunity doesn’t come knocking twice!</p>'
-var joke2 = '<h1><p>Knock Knock!</p><p>Whos there?</p><p>beats.</p><p>Beats who?</p><Beats me!</p>'
-var joke3 = '<h1><p>Knock Knock!</p><p>Whos there?</p><p>Spell.</p><p>Spell who?</p><p>W-H-O</p>'
-*/
 var server = http.createServer(function (request, response) {
   if (request.url === '/') {
     response.end('<h1>Hello! :)</h1>')
   } else if (request.url === '/random-joke') {
     var jokeNumber = Math.random()
     if (jokeNumber < 0.3) {
-      response.end('<h1><p>Knock Knock!</p><p>Who\'s there?</p><p>Opportunity!</p><p>That is impossible. Opportunity doesn\'t come knocking twice!</p></h1>' + '<h2><a href="http://localhost:' + port + '">Welcome Page</a></h2>')
+      response.end('<h1><p>Knock Knock!</p><p>Who\'s there?</p><p>Opportunity!</p><p>That is impossible. Opportunity doesn\'t come knocking twice!</p></h1>' + '<h2><a href="https://squibbsg-kappab1hhmo94jtro3q.herokuapp.com/">Welcome Page</a></h2>')
     } else if (jokeNumber < 0.7) {
-      response.end('<h1><p>Knock Knock!</p><p>Who\'s there?</p><p>beats.</p><p>Beats who?</p><Beats me!</p></h1>' + '<h2><a href="http://localhost:' + port + '">Welcome Page</a></h2>')
+      response.end('<h1><p>Knock Knock!</p><p>Who\'s there?</p><p>beats.</p><p>Beats who?</p><Beats me!</p></h1>' + '<h2><a href="https://squibbsg-kappab1hhmo94jtro3q.herokuapp.com/">Welcome Page</a></h2>')
     } else {
-      response.end('<h1><p>Knock Knock!</p><p>Who\'s there?</p><p>Spell.</p><p>Spell who?</p><p>W-H-O</p></h1>' + '<h2><a href="http://localhost:' + port + '">Welcome Page</a></h2>')
+      response.end('<h1><p>Knock Knock!</p><p>Who\'s there?</p><p>Spell.</p><p>Spell who?</p><p>W-H-O</p></h1>' + '<h2><a href="https://squibbsg-kappab1hhmo94jtro3q.herokuapp.com/">Welcome Page</a></h2>')
     }
   } else if (request.url === '/cuteness') {
-    response.end('<h1>Cutie Kitty!</h1>' + '<img src="http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg" alt="Grey Kitten">' + '<h2><a href="http://localhost:' + port + '">Welcome Page</a></h2>')
+    response.end('<h1>Cutie Kitty!</h1>' + '<img src="http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg" alt="Grey Kitten">' + '<h2><a href="https://squibbsg-kappab1hhmo94jtro3q.herokuapp.com/">Welcome Page</a></h2>')
   } else {
     response.end('<h1>Page Not Found</h1>' + '<h2>The requested URL ' + request.url + ' was not found on this server</h2>')
   }
